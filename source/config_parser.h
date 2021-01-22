21020a07ff80f174c828d8a4a38d9c37776678f5
Graham Riches <graham@attabotics.com>
2021-01-22 09:20:15 -0700

    Smashed together a quick hardware json config and some enums for parsing. Pretty rough, could be a lot better probably

--- source/config_parser.h
+++ source/config_parser.h
@@ -17,15 +17,62 @@
 
 /************************************ Types ********************************************/
 
+namespace Configuration {
+
+enum class HardwareSettings : unsigned {
+    mapping,
+    panel_type,
+    scan_mode,
+    row_address_type,
+    multiplexing
+};
+
+
+enum class DimensionSettings : unsigned {
+    rows,
+    columns
+};
+
+
+enum class ChainingSettings : unsigned {
+    chain_length,
+    parallel_chains
+};
+
+
+enum class PWMSesttings : unsigned {
+    brightness,
+    invert_colors,
+    rgb_sequence,
+    pixel_mapper
+};
+
+
+enum class RefreshSettings : unsigned {
+    show_rate,
+    limit_rate
+};
+
+
+enum class RuntimeSettings : unsigned {
+    slowdown,
+    daemonize,
+    initialize_gpio
+};
+
+
+enum class SlowdownLevel : unsigned {
+    none,
+    one,
+    two,
+    three,
+    four
+};
 
 
 std::optional<rgb_matrix::RGBMatrix::Options> parse_matrix_options(const std::string& data);
 
 
-
-
-
-
-
+};
 
 #endif /* __CONFIG_PARSER_H__ */
