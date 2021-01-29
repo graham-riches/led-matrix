ae2343420384d65688c254fb814fca1451e96362
Graham Riches <graham@attabotics.com>
2021-01-29 09:30:50 -0700

    turns out linking statically fixes the dynamic loading issues..

--- scripts/socket_test.py
+++ scripts/socket_test.py
@@ -11,5 +11,5 @@ import socket
 if __name__ == '__main__':
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.connect(('192.168.1.40', 1234))
-    data = socket.recv(128)
-    print(data.decode('utf-8'))
+    for i in range(5):
+        socket.send('hello from client'.encode())
