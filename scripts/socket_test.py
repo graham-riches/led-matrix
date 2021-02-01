f7cf23054ad1f7b4f5eb14c4d4e3aba061881bff
Graham Riches <graham@attabotics.com>
2021-02-01 05:52:27 -0700

    bumped slowdown option to reduce flickering

--- scripts/socket_test.py
+++ scripts/socket_test.py
@@ -13,8 +13,7 @@ import time
 if __name__ == '__main__':
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.connect(('192.168.1.40', 1234))
-    for i in range(5):
+    for i in range(20):
         lights_on = {'x': i, 'y': i, 'r': 255, 'g': 0, 'b': 0}
         message = '{}\n'.format(json.dumps(lights_on))
-        socket.send(message.encode())        
-        time.sleep(1)
+        socket.send(message.encode())
